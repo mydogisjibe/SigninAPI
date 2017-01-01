be0497f4b5b1e8693fa1ca450548ab8f12ca8d0e
Naomi Sagan <naomi.sagan@gmail.com>
2017-01-01 19:56:10 +0000

    Implement socket

--- client/js/script.js
+++ client/js/script.js
@@ -1,6 +1,171 @@
-function makeVisible(){
-  $(".hideMe").css("visibility","visible");
+/*global $
+global io*/
+function showText(){//show hidden text when a button is pressed
+  $(".hideMe").css("display","block");
 }
-function makeInvisible(){
-  $(".hideMe").css("visibility","collapse");
+function hideText(){//hide hidden text
+  $(".hideMe").css("display","none");
+}
+function showError(error){//display bootstrap alert
+  $(".alert").html("<strong>Sign-in failed!</strong> "+error);
+  $('.alert').css("display","block");
+}
+
+//setting global variables
+var socket = io.connect();
+var sort = 0;
+function submitId(){//submitting a student id to search for the member
+  //hide alert and bottom text
+  $('.alert').css("display","none");
+  hideText();
+  //get student id
+  var id = $("#id").val();
+  if (id==="")showError("ID blank.");
+  else{
+    socket.emit("id",id,function(res){//result found in database
+      if (res==="")showError("ID not found.");
+      else{
+        $("#stu_name").html("Hi "+res.fname+"!");
+        showText();
+        $("#studentSignIn").click(function(){//signing in
+          socket.emit("student signin",{member:res._id,time:Math.floor(Date.now()/1000)});
+        });
+      }
+    });
+  }
+}
+function getStudents(){//display list of members
+  socket.emit("students","",function(list){
+    list = srt(list,sort);//sort by name or student id
+    $("tbody").html("");//clear table
+    for (var i = 0; i < list.length; i++){//loop through students, create a row for each
+      makeRow(i,list[i]);
+    }
+  });
+  function makeRow(j,mem){//add row to student table
+    var row = '<tr id="row'+j+'"><td>'+mem.fname+'</td><td>'+mem.studentId+'</td><td>';
+      row +='<button class = "btn btn-danger margin" id="del'+j+'"><span class="glyphicon glyphicon-trash"></span></button>';//delete button
+      row+='<button class = "btn btn-primary margin" id="edit'+j+'"><span class="glyphicon glyphicon-pencil"></span></button> </td></tr>';//edit button
+      $("tbody").append(row);
+      $("#del"+j).click(function(){//delete a student
+        //Are you SURE you want to delete the student?
+        $("#modaltitle2").html("Are you <em>sure</em> you want to delete "+mem.fname+"?");
+        $("#modal2").modal();
+        $("#delete").click(function(){
+          socket.emit("delete student",mem._id,function(){
+            $("#delete").off();
+            getStudents();//display new table
+          });
+        });
+      });
+      $("#edit"+j).click(function(){//edit student
+        $(".modal-title").html("Edit Student: "+mem.fname);//opions displayed in a modal
+        $("#myModal").modal();
+        $('#chId').click(function(){//changing the student id
+          var id = $('#changeId').val();
+          if (id=="")id = mem.studentId;
+          $("#changeId").val("");
+          socket.emit("update member",{_id:mem._id,member:{fname:mem.fname,studentId:parseInt(id)}},function(){
+            $("#chName").off();//releasing click events for modal buttons 
+            $("#chId").off();
+            $("#myModal").modal('hide');
+            getStudents();//display new table
+          });
+          
+        });
+        $('#chName').click(function(){//changing the name
+          var n = $('#changeName').val();
+          if (n=="")n = mem.fname;
+          $("#changeName").val("");
+          socket.emit("update member",{_id:mem._id,member:{fname:n,studentId:mem.studentId}},function(){
+            $("#chName").off();//releasing click events
+            $("#chId").off();
+            $("#myModal").modal('hide');
+            getStudents();//display new table
+          });
+          
+        });
+        
+      });
+  }
+  
+}
+function getSignins(){//display list of signins
+  socket.emit("signins","",function(list){
+    list = srt(list,2);//sort by date
+    getSignin(0,list);
+  });
+}
+function getSignin(j,list){/*since this function uses socket to get the students' names and ids,
+it cannot simply loop through the signins*/
+    $("tbody").html("");
+      var t = list[j].time;
+      var last = list.length;
+      var id = list[j]._id;
+      socket.emit("obj id",list[j].member,function(student){
+        makeRow(j,student,t,id);
+        
+      });
+      if (j<last-1)getSignin(j+1,list);//get the next signin
+  function makeRow(j,mem,time,id){//create a row on the table for a signin
+    var row = '<tr id="row'+j+'"><td>'+mem.fname+'</td><td>'+mem.studentId+'</td><td>'+timeConverter(time)+'</td><td>';
+    row +='<button class = "btn btn-danger margin" id="del'+j+'"><span class="glyphicon glyphicon-trash"></span></button></td></tr>';//delete button
+    $("tbody").append(row);
+    $("#del"+j).click(function(){//delete button
+        //Are you SURE you want to delete the signin?
+        $("#modaltitle2").html("Are you <em>sure</em> you want to delete this sign-in?");
+        $("#modal2").modal();
+        $("#delete").click(function(){
+          socket.emit("delete signin",id,function(){
+            $("#delete").off();
+            getSignins();//display new table
+          });
+        });
+      });
+  }
+}
+function createStudent(){//create a new student
+  $(".alert").css("display","none");//hide any alerts
+  var name = $("#newFirst").val();
+  var id = parseInt($("#newID").val());
+  if (name==""||id==""){
+    $(".alert").css("display","block");//name or id not entered
+  }
+  else{
+    $("#newFirst").val("");
+    $("#newID").val("");
+    socket.emit("create member", {fname:name,studentId:id},function(){//create member
+      getStudents();//display new table
+    });
+  }
+}
+function select() {//choose how students are sorted
+  sort = document.getElementById("select").selectedIndex;
+  getStudents();//display sorted table
+ }
+function timeConverter(UNIX_timestamp){//convert unix timestamp to date
+  var a = new Date(UNIX_timestamp * 1000);
+  var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+  var year = a.getFullYear();
+  var month = months[a.getMonth()];
+  var date = a.getDate();
+  var hour = a.getHours();
+  var min = a.getMinutes();
+  var sec = a.getSeconds();
+  if (min<10)min = "0"+min.toString();
+  if (sec<10)sec = "0"+sec.toString();
+  var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+  return time;
+}
+function srt(arr, param){//sort object array
+  return arr.sort(function(a, b){
+    switch(param){
+      //sort by fname
+      case 0:return ((a.fname>b.fname)?1:((a.fname==b.fname)?0:-1));
+      //sort by id
+      case 1: return ((a.studentId>b.studentId)?1:((a.studentId==b.studentId)?0:-1));
+      //sort by date
+      case 2: return ((a.time>b.time)?1:((a.time==b.time)?0:-1));
+    }
+  });
 }
--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 // A simple chat server using Socket.IO, Express, and Async.
 //
 var path = require('path');
+var http = require("http");
 
 var express = require('express');
 var bodyParser = require('body-parser');
@@ -11,6 +12,7 @@ var bodyParser = require('body-parser');
 var db = require("./db");
 
 var socket = require('socket.io');
+var ObjectID = require('mongodb').ObjectID; 
 
 //
 // ## SimpleServer `SimpleServer(obj)`
@@ -24,6 +26,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+
+var server = http.createServer(app);
+var io = socket.listen(server);
 
 app.get('/member', function (req, res) {
   db.read("member", {}, function(result){
@@ -76,18 +81,18 @@ app.get("/signin", function(req, res){
     //Find all members with IDs that have a signin, and loop through them.
     //Replace each ID with a member object
     db.read("member", {_id: {$in: members}}, function(result){
-      var responceArray = [];
+      var responseArray = [];
       for(var i=0;i<result.length;i++){
         for(var j=signins.length-1;j>=0;j--){
           if(signins[j].member.equals(result[i]._id)){
             signins[j].member = {fname:result[i].fname};
             //Take out the signin (Since we know who it is) and put it int the responce
-            responceArray.push(signins.splice(j,1)[0]);
+            responseArray.push(signins.splice(j,1)[0]);
           }
         }
       }
       
-      res.send(JSON.stringify(responceArray));
+      res.send(JSON.stringify(responseArray));
     });
     
 
@@ -111,7 +116,70 @@ app.post("/signin", function(req, res){
 
 db.connectDB(function(){
     process.stdout.write("Starting server... ");
-    app.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+    server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
         console.log("Done.");
     });
+});
+
+  //socket
+io.on('connection', function(socket){//socket connected
+  console.log('Socket connected.');
+  
+  socket.on("id",function(id,fn){//student inputs id
+    db.read("member",{"studentId":parseInt(id)},function(res){//find member with that student id
+       console.log(res);
+       //execute callback
+       if (res.length!=0)fn(res[0]);//first member with that id
+       else fn("");//not found
+    });
+  });
+  socket.on("student signin",function(info){///student signs in
+      db.create("signin",[{member:new ObjectID(info.member),time:info.time}], function(result){//create new signin
+        console.log("Sign in!");
+      });
+  });
+  socket.on("students",function(data,fn){//get all members
+    db.read("member", {}, function(result){
+      fn(result);
+    });
+  });
+  socket.on("delete student", function(id,fn){//delete a member
+    db.remove("member", {_id: new ObjectID(id)}, function(result){
+      db.remove("signin",{member:new ObjectID(id)},function(){});//remove all signins from that member
+      fn();
+    });
+  });
+  socket.on("create member", function(member,fn){//create a member
+      db.create("member", [member], function(result){//add to member collection
+        fn();
+      });
+  });
+  socket.on("update member",function(info,fn){//chenge member info
+    db.update("member",{_id:new ObjectID(info._id)}, info.member, function(result){//update document
+      fn();
+    });
+  });
+  socket.on("signins",function(info,fn){//get all signins
+    db.read("signin", {}, function(result){
+      fn(result);
+    });
+  });
+  socket.on("delete signin", function(id,fn){//delete a signin
+    db.remove("signin", {_id: new ObjectID(id)}, function(result){
+      console.log("removed");
+      fn();
+    });
+  });
+  //find a member based on their object id
+  //the object ids are stored in the signin collection, but the names will be displayed
+  socket.on("obj id",function(id,fn){
+    db.read("member",{_id: new ObjectID(id)},function(res){
+       console.log(res);
+       if (res.length!=0){
+         fn(res[0]);
+       }
+       else fn({fname:"Deleted",studentId:00000});//in case the object id is not found
+    });
+  });
+  
 });
